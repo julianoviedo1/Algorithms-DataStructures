91455c855cf12674b585f607dd1a71bf84079661
julian oviedo <julianoviedo1@gmail.com>
2022-11-03 21:40:44 -0300

    added tree traversals

--- src/day1/BTPostOrder.ts
+++ src/day1/BTPostOrder.ts
@@ -1,3 +1,18 @@
-export default function post_order_search(head: BinaryNode<number>): number[] {
+function walk(curr: BinaryNode<number> | null, path: number[]): number[] {
+    // base case
+    if (!curr) {
+        return path;
+    }
 
+    // recurse
+    walk(curr.left, path);
+    walk(curr.right, path);
+    path.push(curr.value);
+    // post
+
+    return path;
+}
+
+export default function post_order_search(head: BinaryNode<number>): number[] {
+    return walk(head, []);
 }
