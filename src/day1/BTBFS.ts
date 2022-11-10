9968613e18e0a970d150536325bd44c189a44847
julian oviedo <julianoviedo1@gmail.com>
2022-11-10 09:15:13 -0300

    Compare BT and depth first search

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,3 +1,23 @@
-export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+// cuando analizas primero el nodo te fijas right left devolves nodo
+// te fijas del right el right left y devolves el nodo
+// te fijas del right el right left y devolves el node
+// cuando encontras el numero buscado retornas true
 
+export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+    const q: (BinaryNode<number> | null)[] = [head];
+    while (q.length) {
+        const curr = q.shift() as BinaryNode<number> | undefined | null;
+        if (!curr) {
+            continue;
+        }
+
+        // search
+        if (curr.value === needle) {
+            return true;
+        }
+
+        q.push(curr.left);
+        q.push(curr.right);
+    }
+    return false;
 }
--- src/day1/CompareBinaryTrees.ts
+++ src/day1/CompareBinaryTrees.ts
@@ -1,3 +1,25 @@
-export default function compare(a: BinaryNode<number> | null, b: BinaryNode<number> | null): boolean {
+// comparing trees with each node element.
 
+export default function compare(
+    a: BinaryNode<number> | null,
+    b: BinaryNode<number> | null,
+): boolean {
+    // Base cases
+    // this is a structural check
+    if (a === null && b === null) {
+        return true;
+    }
+
+    // this is a structural check
+    if (a === null || b === null) {
+        return false;
+    }
+
+    // this is a value check
+    if (a.value !== b.value) {
+        return false;
+    }
+
+    // recursion
+    return compare(a?.left, b?.left) && compare(a?.right, b?.right);
 }
